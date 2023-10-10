e03c333681fef94486f3bb4b976bcaef27fcfb74
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-10-10 09:35:23 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/insanetask/InsaneTaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.insanetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaneTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsaneTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/insanetask/InsaneTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.insanetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaneTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
